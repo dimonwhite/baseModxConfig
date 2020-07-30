faed903373dfa02ead5a12425f2241fef2c0210b
dimonwhite <gen-tver@yandex.ru>
2020-07-30 13:50:13 +0300

    fix: delete cache

--- core/cache/includes/elements/modplugin/12.include.cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($modx->context->get('key') == 'mgr') {return;}
-$url = $_SERVER['REQUEST_URI'];
-if (preg_match('~[A-Z]~', $url)) {
-   $modx->sendRedirect(strtolower($url));
-}
-return;
--- core/cache/lexicon_topics/lexicon/ru/core/system_events.cache.php
+++
@@ -1,9 +0,0 @@
-<?php  return array (
-  'clear' => 'Очистить',
-  'error_log' => 'Журнал ошибок',
-  'error_log_desc' => 'Здесь находится журнал ошибок MODX Revolution:',
-  'error_log_download' => 'Скачать журнал ошибок ([[+size]])',
-  'error_log_too_large' => 'Журнал ошибок <em>[[+name]]</em> слишком большой для просмотра. Вы можете скачать журнал, нажав на кнопку ниже.',
-  'system_events' => 'Системные события',
-  'priority' => 'Приоритет',
-);
